cb739b3eacaac22378e37945571c8bb8162f7a70
Björn Benouarets <70456959+bbenouarets@users.noreply.github.com>
2023-03-17 21:35:17 +0100

    Set version of pip package to 1.0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ SRC_DIR = "src"
 
 setup(
     name="dynami",
-    version="1.0.2",
+    version="1.0.3",
     author="ByteSentinel",
     author_email="info@bytesentinel.io",
     description="Module to dynamically update DNS records from multiple services like Hetzner, Amazon, Microsoft... ",
